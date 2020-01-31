db77280545427312aeab706d0bdd91c4354bdcb8
emilionito616 <emilionito9@gmail.com>
2020-01-31 16:55:16 -0600

    Pipe para el porcentaje

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre = "Emilio";
+
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+
+  PI = Math.PI;
+
+  a = 0.234;
 }
